78bd0f3781bb125cf16af65afbc03e0484c6901b
TimB44 <tblamires22@gmail.com>
2024-12-28 20:18:02 -0700

    Implement and test passphrase_generator function

--- src/passphrase_generator.rs
+++ src/passphrase_generator.rs
@@ -1,8 +1,10 @@
 use core::str;
 
+use rand::Rng;
+
 const WORD_COUNT: usize = 9_800;
-const WORDS_FILE: &[u8; 74376] = include_bytes!("../res/words.txt");
-const WORDS: [&'static str; WORD_COUNT] = split_words(WORDS_FILE);
+const WORDS: [&'static str; WORD_COUNT] = split_words(include_bytes!("../res/words.txt"));
+const PASSPHRASE_GENERATOR_DELIMTER: &str = "-";
 
 const fn split_words(mut input: &[u8]) -> [&str; WORD_COUNT] {
     let mut words: [&str; WORD_COUNT] = [""; WORD_COUNT];
@@ -25,11 +27,31 @@ const fn split_words(mut input: &[u8]) -> [&str; WORD_COUNT] {
     words
 }
 
+/// Generates a random passphrase with length number of words joined by a hypen
+pub fn generate_passphrase(length: usize) -> String {
+    let mut rand = rand::thread_rng();
+    (0..length)
+        .into_iter()
+        .map(|cur| {
+            if cur == length - 1 {
+                [WORDS[rand.gen_range(0..WORD_COUNT)], ""]
+            } else {
+                [
+                    WORDS[rand.gen_range(0..WORD_COUNT)],
+                    PASSPHRASE_GENERATOR_DELIMTER,
+                ]
+            }
+        })
+        .flatten()
+        .collect()
+}
+
 #[cfg(test)]
 mod passphrase_generator_tests {
     use core::str;
 
-    use super::{WORDS, WORDS_FILE};
+    use super::{generate_passphrase, PASSPHRASE_GENERATOR_DELIMTER, WORDS};
+    const WORDS_FILE: &[u8; 74376] = include_bytes!("../res/words.txt");
 
     #[test]
     fn words_valid_utf8() {
@@ -43,5 +65,23 @@ mod passphrase_generator_tests {
         let words: Vec<_> = all_words.lines().collect();
 
         assert_eq!(&WORDS, words.as_slice());
+    }
+
+    #[test]
+    fn fuzz() {
+        for length in 1..800 {
+            let pf = generate_passphrase(length);
+
+            // should have eactlye length - 1 hyphens
+            assert_eq!(
+                pf.chars()
+                    .filter(|&x| x == PASSPHRASE_GENERATOR_DELIMTER.chars().next().unwrap())
+                    .count(),
+                length - 1
+            );
+
+            // generated passphrase should not have any white space
+            assert!(!pf.contains(|c: char| c.is_whitespace()));
+        }
     }
 }
